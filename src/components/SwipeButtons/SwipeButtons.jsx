002fef9387115a034bb80263fcd8c78458f253bc
vitor <vho2009@hotmail.com>
2020-12-10 20:58:45 -0300

    alterando layout da pagina home

--- src/components/SwipeButtons/SwipeButtons.jsx
+++ src/components/SwipeButtons/SwipeButtons.jsx
@@ -9,25 +9,27 @@ import IconButton from '@material-ui/core/IconButton';
 
 function SwipeButtons() {
     return (
-        <div className="container">
-            <IconButton className="repeat">
-                <ReplayIcon fontSize="large" />
-            </IconButton>
-            <IconButton className="left">
-                <CloseIcon fontSize="large" />
-            </IconButton>
-            <IconButton className="start">
-                <StarRateIcon fontSize="large" />
+        <div className={"swipe-buttons"}>
+            <div className="container">
+                <IconButton className="repeat">
+                    <ReplayIcon fontSize="large"/>
+                </IconButton>
+                <IconButton className="left">
+                    <CloseIcon fontSize="large"/>
+                </IconButton>
+                <IconButton className="start">
+                    <StarRateIcon fontSize="large"/>
 
-            </IconButton>
-            <IconButton className="right">
-                <FavoriteIcon fontSize="large" />
-            </IconButton>
+                </IconButton>
+                <IconButton className="right">
+                    <FavoriteIcon fontSize="large"/>
+                </IconButton>
 
-            <IconButton className="light">
-                <FlashOnIcon fontSize="large" />
-            </IconButton>
+                <IconButton className="light">
+                    <FlashOnIcon fontSize="large"/>
+                </IconButton>
 
+            </div>
         </div>
     )
 }
--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -1,62 +1,65 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import logo from "../../assets/img/logo.png";
 import "./Login.css";
 import TextField from "@material-ui/core/TextField";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import firebase from "firebase/app";
-import { useHistory } from "react-router-dom";
+import {useHistory} from "react-router-dom";
 
 function Login(props) {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const history = useHistory()
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const history = useHistory()
 
-  const handleSubmit = () => {
-    console.log(email);
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then(({user}) => {
-        if (user) {
-          localStorage.setItem("user_id",user.uid);
-          console.log("Estou salvando",localStorage.getItem("user_id"))
-          history.push("/perfis");
-        }
-      })
-      .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode + " - " + errorMessage);
-      });
-  };
+    const handleSubmit = () => {
+        console.log(email);
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .then(({user}) => {
+                if (user) {
+                    localStorage.setItem("user_id", user.uid);
+                    console.log("Estou salvando", localStorage.getItem("user_id"))
+                    history.push("/perfis");
+                }
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode + " - " + errorMessage);
+            });
+    };
 
-  return (
-    <div className="login container">
-      <img src={logo} alt="banginder" />
-      <TextField
-        className="input"
-        type="text"
-        id="standard-basic"
-        label="Digite seu email"
-        onChange={(event) => setEmail(event.target.value)}
-      />
-      <TextField
-        className="input"
-        type="password"
-        id="standard-basic"
-        label="Digite sua senha"
-        onChange={(event) => setPassword(event.target.value)}
-      />
-      <Button className="button" variant="outlined" onClick={handleSubmit}>
-        Login
-      </Button>
-      <Button className="button" variant="outlined">
-        <Link className="link" to="/perfis">
-          Registrar
-        </Link>
-      </Button>
-    </div>
-  );
+    return (
+        <div className={"login"}>
+            <div className="container">
+                <img src={logo} alt="banginder"/>
+                <TextField
+                    className="input"
+                    type="text"
+                    id="standard-basic"
+                    label="Digite seu email"
+                    onChange={(event) => setEmail(event.target.value)}
+                />
+                <TextField
+                    className="input"
+                    type="password"
+                    id="standard-basic"
+                    label="Digite sua senha"
+                    onChange={(event) => setPassword(event.target.value)}
+                />
+                <Button className="button" variant="outlined" onClick={handleSubmit}>
+                    Login
+                </Button>
+                <Button className="button" variant="outlined">
+                    <Link className="link" to="/perfis">
+                        Registrar
+                    </Link>
+                </Button>
+            </div>
+        </div>
+    );
 }
+
 export default Login;
