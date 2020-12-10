3dde6625c578f1cc8973e392fdf3bc658d705d7c
Jose Neto <zenetoshl@gmail.com>
2020-12-10 13:18:55 -0300

    firebase auth

--- src/App.jsx
+++ src/App.jsx
@@ -7,44 +7,59 @@ import TinderCards from "./containers/TinderCard/TinderCards";
 import Registrar from "./containers/Registrar/Registrar"
 import SwipeButtons from './components/SwipeButtons/SwipeButtons';
 import Chat from "./containers/Chat/Chat";
-import ChatScreen from './containers/ChatScreen/ChatScreen'
+import ChatScreen from './containers/ChatScreen/ChatScreen';
+import firebase from "firebase";
+import {FirebaseAuthProvider} from "@react-firebase/auth";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBrKw6x2SiTa511W3tBRNF1jtfkNXAljrY",
+  authDomain: "banginder-59b1c.firebaseapp.com",
+  databaseURL: "https://banginder-59b1c.firebaseio.com",
+  projectId: "banginder-59b1c",
+  storageBucket: "banginder-59b1c.appspot.com",
+  messagingSenderId: "577753223059",
+  appId: "1:577753223059:web:2355a8ad6f3bbc6e916266"
+};
 function App() {
   return (
+    
     <div className="App">
+      <FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
+        <Router>
+          <Switch>
 
-      <Router>
-        <Switch>
+            <Route path="/chat/:person">
+              <Header backButton="/chat" />
+              <ChatScreen />
+            </Route>
 
-          <Route path="/chat/:person">
-            <Header backButton="/chat" />
-            <ChatScreen />
-          </Route>
+            <Route path="/chat">
+              <Header backButton="/perfis" />
+              <Chat />
+            </Route>
 
-          <Route path="/chat">
-            <Header backButton="/perfis" />
-            <Chat />
-          </Route>
+            <Route path="/registrar">
+              <Registrar />
+            </Route>
 
-          <Route path="/registrar">
-            <Registrar />
-          </Route>
+            <Route path="/perfil">
+              <h1>Perfil</h1>
+            </Route>
 
-          <Route path="/perfil">
-            <h1>Perfil</h1>
-          </Route>
+            <Route path="/perfis">
+              <Header />
+              <TinderCards />
+              <SwipeButtons />
+            </Route>
 
-          <Route path="/perfis">
-            <Header />
-            <TinderCards />
-            <SwipeButtons />
-          </Route>
-
-          <Route path="/">
-            <Login />
-          </Route>
-        </Switch>
-      </Router>
+            <Route path="/">
+              <Login />
+            </Route>
+          </Switch>
+        </Router>
+      </FirebaseAuthProvider>
     </div>
+   
   );
 }
 
--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -1,33 +1,65 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import logo from "../../assets/img/logo.png";
-import "./Login.css"
-import TextField from '@material-ui/core/TextField';
-import { Link } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
+import "./Login.css";
+import TextField from "@material-ui/core/TextField";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import firebase from "firebase/app";
 
 class Login extends Component {
-    state = {
-        email: "",
-        password: ""
-    };
+  state = {
+    email: "",
+    password: "",
+  };
+  handleSubmit = (e) => {
+    const { email, password } = this.state;
+    console.log(email);
+    firebase.auth().signInWithEmailAndPassword(email, password);
+  };
 
-    render() {
-        return (
-            <div className="container">
-                <img src={logo} alt="banginder" />
-                <TextField className="input" type="text" id="standard-basic" label="Digite seu email" />
-                <TextField className="input" type="password" id="standard-basic" label="Digite sua senha" />
-                <Button className="button" variant="outlined">
-                    <Link className="link" to="/perfis">Login</Link>
-                </Button>
-                <Button className="button" variant="outlined">
-                    <Link className="link" to="/registrar">Registrar</Link>
-                </Button>
+  handleChangeEmail = (event) => {
+    console.log(event.target.value);
+    this.setState({ email: event.target.value});
+    console.log(this.state.email);
+  };
 
+  handleChangePassword = (event) => {
+    this.setState({ password: event.target.value});
+  };
 
-            </div>
-        )
-    }
-
+  render() {
+    return (
+      <div className="container">
+          <img src={logo} alt="banginder" />
+          <TextField
+            className="input"
+            type="text"
+            id="standard-basic"
+            label="Digite seu email"
+            onChange={this.handleChangeEmail}
+          />
+          <TextField
+            className="input"
+            type="password"
+            id="standard-basic"
+            label="Digite sua senha"
+            onChange={this.handleChangePassword}
+          />
+          <Button
+            className="button"
+            variant="outlined"
+            onClick={this.handleSubmit}
+          >
+              Login
+            {/*<Link className="link" to="/perfis">Login</Link>*/}
+          </Button>
+          <Button className="button" variant="outlined">
+            <Link className="link" to="/registrar">
+              Registrar
+            </Link>
+          </Button>
+      </div>
+    );
+  }
 }
 export default Login;
