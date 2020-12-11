92a6742a771599adf8428070130ccc09a48d8543
vitor <vho2009@hotmail.com>
2020-12-10 23:02:37 -0300

    Subindo ultimas alteracoes

--- src/containers/TinderCard/TinderCards.jsx
+++ src/containers/TinderCard/TinderCards.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import SwipeButtons from "../../components/SwipeButtons/SwipeButtons";
 import Header from "../../components/Header/Header";
 import TinderCard from "react-tinder-card";
@@ -6,111 +6,136 @@ import firebase from "firebase/app";
 import "./TinderCard.css";
 
 function TinderCards() {
-  const [people, setPeople] = useState([
-  ]);
-  const userUid = localStorage.getItem("user_id");
-  
-  const handleCardLeft = (direction) => {
-    const enemyUid = people[0].uid;
-    if (direction == "left") {
-      console.log(people[0].name);
-      firebase
-        .firestore()
-        .collection("Hate")
-        .doc(userUid)
-        .collection("HateList")
-        .doc(enemyUid)
-        .set({
-          message: [
-            {
-              text: "",
-              date: "",
-            },
-          ],
-        });
+    const [people, setPeople] = useState([]);
+    const userUid = localStorage.getItem("user_id")
+
+    function getAge(dateString) {
+        const today = new Date();
+        const birthDate = new Date(dateString);
+        let age = today.getFullYear() - birthDate.getFullYear();
+        const m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age;
     }
-    setPeople((people) => [...people.slice(1)]);
-  };
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    const firestore = firebase.firestore();
-    setLoading(true);
-    firestore
-      .collection("registerInfo")
-      .get()
-      .then((snap) => {
-        const hateList = snap.docs
-          .map((e) => e.id)
-          .filter((id) => id !== userUid);
+
+    const handleCardLeft = (direction) => {
+        const enemyUid = people[0].uid;
+        if (direction == "left") {
+            console.log(people[0].name);
+            firebase
+                .firestore()
+                .collection("Hate")
+                .doc(userUid)
+                .collection("HateList")
+                .doc(enemyUid)
+                .set({
+                    message: [
+                        {
+                            text: "",
+                            date: "",
+                        },
+                    ],
+                });
+        }
+        setPeople((people) => [...people.slice(1)]);
+    };
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        const firestore = firebase.firestore();
+        setLoading(true);
         firestore
-          .collection("Hate")
-          .doc(userUid)
-          .collection("HateList")
-          .get()
-          .then((snap) => {
-            const ids = snap.docs.map((e) => e.id);
-            hateList
-              .filter((s) => !ids.includes(s))
-              .forEach((uid) =>
+            .collection("registerInfo")
+            .get()
+            .then((snap) => {
+                const hateList = snap.docs
+                    .map((e) => e.id)
+                    .filter((id) => id !== userUid);
                 firestore
-                  .collection("registerInfo")
-                  .doc(uid)
-                  .get()
-                  .then((snap) => {
-                    const data = snap.data();
-                    setPeople((people) => [
-                      {
-                        uid: data.uid,
-                        url: data.url,
-                        bio: data.bio,
-                        dislikes: data.dislikes,
-                        likes: data.likes,
-                        name: data.name + " " + data.surname,
-                      },
-                      ...people,
-                    ]);
-                  })
-              )
-              setLoading(false);
-          });
-      })
-      .catch(() => setLoading(false));
-      return () => {
-        setPeople([])
-      }
-  }, []);
-  //setPeople([...people, 'athena', 'josy', 'vito'])
-  if (loading) {
-    return <div>Carregando...</div>;
-  } else
-    return (
-      <React.Fragment>
-        <Header />
-        <div>
-          <div className="tinderContainer">
-            {people
-              .slice(0)
-              .reverse()
-              .map((person) => (
-                <TinderCard
-                  onCardLeftScreen={handleCardLeft}
-                  className="swipe"
-                  key={person.uid}
-                  preventSwipe={["up", "down"]}
-                >
-                  <div
-                    className="card"
-                    style={{ backgroundImage: `linear-gradient(rgb(0 0 0 / 0%), rgb(102 65 121 / 52%)),url(${person.url})` }}
-                    // linear-gradient(rgba(0, 0, 0, 0), rgb(74 65 121 / 52%))
-                  >
-                    <h3>{person.name}</h3>
-                  </div>
-                </TinderCard>
-              ))}
-          </div>
-        </div>
-      </React.Fragment>
-    );
+                    .collection("Hate")
+                    .doc(userUid)
+                    .collection("HateList")
+                    .get()
+                    .then((snap) => {
+                        const ids = snap.docs.map((e) => e.id);
+                        hateList
+                            .filter((s) => !ids.includes(s))
+                            .forEach((uid) =>
+                                firestore
+                                    .collection("registerInfo")
+                                    .doc(uid)
+                                    .get()
+                                    .then((snap) => {
+                                        const data = snap.data();
+                                        console.log(data)
+                                        setPeople((people) => [
+                                            {
+                                                ...data
+                                            },
+                                            ...people,
+                                        ]);
+                                    })
+                            )
+                        setLoading(false);
+                    });
+            })
+            .catch(() => setLoading(false));
+        return () => {
+            setPeople([])
+        }
+    }, []);
+    //setPeople([...people, 'athena', 'josy', 'vito'])
+    if (loading) {
+        return <div>Carregando...</div>;
+    } else
+        return (
+            <React.Fragment>
+                <Header/>
+                <div>
+                    <div className="tinderContainer">
+                        {people
+                            .slice(0)
+                            .reverse()
+                            .map((person) => (
+                                <TinderCard
+                                    onCardLeftScreen={handleCardLeft}
+                                    className="swipe"
+                                    key={person.uid}
+                                    preventSwipe={["up", "down"]}
+                                >
+                                    <div
+                                        className="card"
+
+                                    >
+                                        <div className={"card-image"}
+                                             style={person.gender === "Homem" ?
+                                                 {backgroundImage: ` linear-gradient(rgba(0, 0, 0, 0), rgb(74 65 121 / 52%)),url(${person.url})`}
+                                                 : {backgroundImage: `linear-gradient(rgb(0 0 0 / 0%), rgb(102 65 121 / 52%)),url(${person.url})`}
+                                             }>
+                                            <h3>{person.name}, {getAge(person.bDay)}</h3>
+                                        </div>
+                                        <div className={"infos"}>
+                                            <h3>Bio: {person.bio}</h3>
+                                        </div>
+                                        <div className={"Gostos"}>
+                                            <h3>Bio: {person.deslikes}</h3>
+                                        </div>
+                                        <div className={"Odeios"}>
+                                            <h3>Bio: {person.like}</h3>
+                                        </div>
+
+
+                                    </div>
+
+
+                                </TinderCard>
+                            ))}
+                    </div>
+                </div>
+                <SwipeButtons/>
+            </React.Fragment>
+        );
 }
 
 export default TinderCards;
